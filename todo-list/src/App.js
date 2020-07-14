fa99dae08b3980f20ca5741708cd8937889b1e38
Dumitru Stefirta <dimastefirta@gmail.com>
2020-07-14 15:53:58 +0300

    On branch master

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToDo from './TodoApp';
+// import ToDo from './TodoApp';
 
 class App extends Component {
   constructor(props) {
